e42eee6847db0eee8f1b3430dad27e2f6de6cefe
Andrei Savelyeu <raikaat@mail.ru>
2020-07-29 20:19:12 +0300

    Add task2

---
+++ src/task2.js
@@ -0,0 +1,18 @@
+const csv = require('csvtojson');
+const fs = require('fs');
+
+const errorCb = err => {
+    console.error(err);
+};
+
+const successCb = () => {
+    console.log('Converted successfully');
+};
+
+const writeStream = fs.createWriteStream('src/output.txt');
+
+csv()
+    .fromFile('src/csv/nodejs-hw1-ex1.csv')
+    .subscribe((data) => {
+        writeStream.write(`${JSON.stringify(data)}\n`);
+    }, errorCb, successCb)
