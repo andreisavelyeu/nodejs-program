7efae431afb985484bdc5b9e55b820636ac072cc
Andrei Savelyeu <andreisavelyeu@gmail.com>
2020-08-03 12:49:33 +0300

    Refactor code using pipeline

--- src/task2.js
+++ src/task2.js
@@ -1,18 +1,16 @@
-import { csv } from 'csvtojson';
 import fs from 'fs';
+import { pipeline } from 'stream';
+import { csv } from 'csvtojson';
 
-const errorCb = err => {
-    console.error(err);
-};
-
-const successCb = () => {
-    console.log('Converted successfully');
-};
-
-const writeStream = fs.createWriteStream('src/output.txt');
-
-csv()
-    .fromFile('src/csv/nodejs-hw1-ex1.csv')
-    .subscribe(data => {
-        writeStream.write(`${JSON.stringify(data)}\n`);
-    }, errorCb, successCb)
+pipeline(
+    fs.createReadStream('src/csv/nodejs-hw1-ex1.csv'),
+    csv(),
+    fs.createWriteStream('src/output.txt'),
+    (err) => {
+        if (err) {
+        console.error('Pipeline failed.', err);
+        } else {
+        console.log('Pipeline succeeded.');
+        }
+    }
+);
