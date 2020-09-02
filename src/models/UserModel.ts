1ee8c5c891bde8dba1f0cc4af438b1a91a97ecc1
Andrei Savelyeu <andreisavelyeu@gmail.com>
2020-09-02 23:01:24 +0300

    Resolve first part of hw3

---
+++ src/models/UserModel.ts
@@ -0,0 +1,29 @@
+import { DataTypes, UUIDV4 } from 'sequelize';
+import { sequelize } from './index';
+
+export const UserModel = sequelize.define('User', {
+    login: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+        unique: true
+    },
+    age: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    isDeleted: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false
+    }
+});
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,7 +1,3 @@
-export type UserList = {
-    [id: string]: User;
-};
-
 export type User = {
     id: string;
     login: string;
